e783ae276df56e7ace50d5ccf33cfc42efc9389c
Matt Swensen <matt@mjswensen.com>
2022-05-31 02:44:34 +0000

    Add banner styles

---
+++ src/lib/Banner.svelte
@@ -0,0 +1,38 @@
+<script lang="ts" context="module">
+  export type Type = 'success' | 'error' | 'warning' | 'info';
+</script>
+
+<script lang="ts">
+  export let type: Type = 'info';
+</script>
+
+<div class={'banner ' + type}>
+  <slot />
+</div>
+
+<style>
+  .banner {
+    padding: var(--scale-2) var(--scale-4);
+    border-radius: var(--size-border-radius-large);
+  }
+  .success {
+    background-color: var(--green-dark);
+    color: var(--green-light);
+    border: var(--size-border-width) solid var(--green-light-25);
+  }
+  .error {
+    background-color: var(--red-dark);
+    color: var(--red-light);
+    border: var(--size-border-width) solid var(--red-light-25);
+  }
+  .warning {
+    background-color: var(--yellow-dark);
+    color: var(--yellow-light);
+    border: var(--size-border-width) solid var(--yellow-light-25);
+  }
+  .info {
+    background-color: var(--blue-dark);
+    color: var(--blue-light);
+    border: var(--size-border-width) solid var(--blue-light-25);
+  }
+</style>
--- src/lib/UnderConstruction.svelte
+++ src/lib/UnderConstruction.svelte
@@ -1,1 +1,20 @@
-<p>🚧 This tool is still under construction. 🚧</p>
+<script>
+  import Banner from './Banner.svelte';
+</script>
+
+<section>
+  <Banner type={'info'}>
+    🚧 This tool is still under construction. Check back soon, or follow the <a
+      href="https://github.com/mjswensen/proutils.dev">GitHub repository</a
+    > for updates.
+  </Banner>
+</section>
+
+<style>
+  section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
