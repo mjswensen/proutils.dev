149c86cac43ef7f2d4a5b62d0c09294d5399180b
Matt Swensen <matt@mjswensen.com>
2022-05-31 02:44:41 +0000

    Add simple URL tool

--- src/routes/tools/encode-decode/url.svelte
+++ src/routes/tools/encode-decode/url.svelte
@@ -1,20 +1,74 @@
 <script lang="ts">
-  let raw = '';
+  import TextField from '$lib/TextField.svelte';
+
+  let decoded = '';
   let encoded = '';
+  let encodedWarning = '';
 
-  // TODO: try/catch and display error
-  function setRaw(event) {
-    const input = event.target!.value;
-    raw = input;
-    encoded = encodeURIComponent(input);
+  function onDecodedInput(value: string) {
+    decoded = value;
+    encoded = encodeURIComponent(value);
+    encodedWarning = '';
   }
 
-  function setEncoded(event) {
-    const input = event.target.value;
-    encoded = input;
-    raw = decodeURIComponent(input);
+  function onEncodedInput(value: string) {
+    try {
+      encoded = value;
+      decoded = decodeURIComponent(value);
+      encodedWarning = '';
+    } catch (err) {
+      decoded = '';
+      encodedWarning = "That doesn't look like URL-encoded data.";
+    }
   }
+
+  $: encodedWarningAction = encodedWarning
+    ? {
+        text: 'Encode instead',
+        action: () => {
+          decoded = encoded;
+          onDecodedInput(decoded);
+        },
+      }
+    : null;
 </script>
 
-<textarea on:input={setRaw}>{raw}</textarea>
-<textarea on:input={setEncoded}>{encoded}</textarea>
+<div class="layout">
+  <header>
+    <h1>URL</h1>
+    <p>
+      Encode strings for use in URLs, or decode URL-encoded data. For advanced
+      URL parsing (including query string parameters), see <a
+        href="/tools/parse/url">the URL parse tool</a
+      >.
+    </p>
+  </header>
+  <TextField
+    multiline
+    placeholder="Decoded data..."
+    value={decoded}
+    onInput={onDecodedInput}
+  />
+  <TextField
+    multiline
+    placeholder="Encoded data..."
+    bind:value={encoded}
+    onInput={onEncodedInput}
+    warning={encodedWarning}
+    warningAction={encodedWarningAction}
+  />
+</div>
+
+<style>
+  .layout {
+    height: 100%;
+    padding: var(--scale-4);
+    display: grid;
+    gap: var(--scale-4);
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto 1fr;
+  }
+  header {
+    grid-column: 1 / 3;
+  }
+</style>
